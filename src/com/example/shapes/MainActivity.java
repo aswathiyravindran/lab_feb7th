b421d7dc2ed823120d108b178d44ab2e76d39034
ITLAB <ITLAB@IT211>
2017-02-07 13:45:24 +0530

    Third

--- src/com/example/shapes/MainActivity.java
+++ src/com/example/shapes/MainActivity.java
@@ -1,35 +1,29 @@
 package com.example.shapes;
 
 import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
+	ShapeView shapeview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+public class ShapeView extends View{
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	public ShapeView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
+   
+    
 }
