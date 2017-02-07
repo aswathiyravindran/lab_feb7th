4fa3578b5dfe005018b2f346bf56f6514062cce4
ITLAB <ITLAB@IT211>
2017-02-07 14:43:54 +0530

    Seventh

--- src/com/example/shapes/SecondActivity.java
+++ src/com/example/shapes/SecondActivity.java
@@ -2,28 +2,63 @@ package com.example.shapes;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
 
 public class SecondActivity extends Activity{
 	ShapeView shapeview;
+	String data;
 	protected void onCreate(Bundle  savedInstanceState)
 	{
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.second_activity);
 	Intent i=getIntent();
-	String data_main= i.getStringExtra("username");
+	data= i.getStringExtra("username");
 	
 	}
 	
-	public class ShapeView extends View
-	{
-
-		public ShapeView(Context context) {
-			super(context);
-			// TODO Auto-generated constructor stub
-		}
-		
-	}
-	
+	private class ShapeView extends View{  
+        public ShapeView(Context context){  
+            super(context);  
+        }  
+  
+        @Override protected void onDraw(Canvas canvas) {  
+            super.onDraw(canvas);  
+  
+            // custom drawing code here  
+            Paint paint = new Paint();  
+            paint.setStyle(Paint.Style.FILL);  
+  
+            // make the entire canvas white  
+            paint.setColor(Color.WHITE);  
+            canvas.drawPaint(paint);  
+              
+            // draw blue circle with anti aliasing turned off  
+            paint.setAntiAlias(false);  
+            paint.setColor(Color.BLUE);  
+            canvas.drawCircle(20, 20, 15, paint);  
+  
+            // draw green circle with anti aliasing turned on  
+            paint.setAntiAlias(true);  
+            paint.setColor(Color.GREEN);  
+            canvas.drawCircle(60, 20, 15, paint);  
+  
+            // draw red rectangle with anti aliasing turned off  
+            paint.setAntiAlias(false);  
+            paint.setColor(Color.RED);  
+            canvas.drawRect(100, 5, 200, 30, paint);  
+                           
+            // draw the rotated text  
+            canvas.rotate(-45);  
+                      
+            paint.setStyle(Paint.Style.FILL);  
+            canvas.drawText("Graphics Rotation", 40, 180, paint);  
+              
+            //undo the rotate  
+            canvas.restore();  
+        }  
+    }  
 }
